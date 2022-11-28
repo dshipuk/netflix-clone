5c13c1cf32c7df27db687f35e0d12019e13d2961
dshipuk <dshipuk@yahoo.com>
2022-11-28 09:44:23 -0800

    Currently I have the banner working the netflix logo and user avatar styled out and will be working on completing the rows sections for movies

---
+++ src/Row.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./Row.css";
+
+const Rows = () => {
+  return <div className="Row">
+    
+  </div>;
+};
+
+export default Rows;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC7lUAkbX6qLfeHi8TsXdQLecfJV6mG-5A",
+  authDomain: "netflix-clone-1d26c.firebaseapp.com",
+  projectId: "netflix-clone-1d26c",
+  storageBucket: "netflix-clone-1d26c.appspot.com",
+  messagingSenderId: "747696291413",
+  appId: "1:747696291413:web:fadd4684f07d6686bc9edc",
+};
+
+export default firebaseConfig;
