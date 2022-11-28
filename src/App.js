ac7a7737d88dcc4a8b5e546957eba36082df75a5
dshipuk <dshipuk@yahoo.com>
2022-11-28 12:44:53 -0800

    completed login screen component and working on a SignUpScreen component now

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./App.css";
 
-import HomeScreen from "./HomeScreen";
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 function App() {
+  const user = null;
+  
   return (
     <div className="App">
-      <HomeScreen />
+      <Router>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Routes>
+            <Route exact path="/" element={<HomeScreen />} />
+          </Routes>
+        )}
+      </Router>
     </div>
   );
 }
---
+++ src/screens/SignUpScreen.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./SignUpScreen.css";
+
+const SignUpScreen = () => {
+  return <div className="signupScreen"></div>;
+};
+
+export default SignUpScreen;
