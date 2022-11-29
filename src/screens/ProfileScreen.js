36fa5da76b6353ad422594e2a3198cdf69062c6c
dshipuk <dshipuk@yahoo.com>
2022-11-29 13:35:34 -0800

    fixed logout call dispatch in app.js and added dispatch to dep array on useEffect

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,12 +1,37 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice";
+import { auth } from "../firebase";
 import Nav from "../Nav";
 
 import "./ProfileScreen.css";
 
 const ProfileScreen = () => {
+  const user = useSelector(selectUser);
+
   return (
     <div className="profileScreen">
       <Nav />
+      <div className="profileScreen_body">
+        <h1>Edit Profile</h1>
+        <div className="profileScreen_info">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117"
+            alt="avatar"
+          />
+        </div>
+        <div className="profileScreen_details">
+          <h2>{user.email}</h2>
+          <div className="profileScreen_plans">
+            <button
+              onClick={() => auth.signOut()}
+              className="profileScreen_signOut"
+            >
+              Sign Out
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
